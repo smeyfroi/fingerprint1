8f3b3874a8bc5d47163266c40359ff87644607e5
smeyfroi <steve.meyfroidt@gmail.com>
2025-10-28 21:55:07 +0000

    Simplify synth to focus on agency and smoothing

--- src/synthSoftCircle.cpp
+++ src/synthSoftCircle.cpp
@@ -14,6 +14,7 @@ using namespace ofxMarkSynth;
 std::shared_ptr<Synth> createSynthSoftCircle(glm::vec2 size) {
   auto synthPtr = std::make_shared<Synth>("SynthSoftCircle", ModConfig {}, START_PAUSED, size);
 
+  // >>> AUDIO, CLUSTERS AND PALETTE
   std::shared_ptr<AudioDataSourceMod> audioDataSourceModPtr = std::static_pointer_cast<AudioDataSourceMod>(synthPtr->addMod<AudioDataSourceMod>("Audio Source", {
     {"MinPitch", "50.0"}, // Tuning: manual variable. Has large effect on point positions etc
     {"MaxPitch", "800.0"},//"1500.0"}, // Tuning: manual variable. Has large effect on point positions etc
@@ -27,16 +28,6 @@ std::shared_ptr<Synth> createSynthSoftCircle(glm::vec2 size) {
     {"MaxZeroCrossingRate", "15.0"}, // how this get set? pre-performance tuning?. Has large effect on palette etc
   }, MIC_DEVICE_NAME, RECORD_AUDIO, RECORDING_PATH, ROOT_SOURCE_MATERIAL_PATH));
   
-  auto smearModPtr = synthPtr->addMod<SmearMod>("Smear", {
-    {"Translation", "0.0, 0.0"}, // could be variable but unlikely to be useful
-    {"MixNew", "0.7"}, // 0.3 (slow) -> 0.9 (fast); could be variable. When this slows down, the multipliers become useful so they are coupled to some extent
-    {"AlphaMultiplier", "0.997"}, // fades fast when lower; could be variable with limits
-    {"Field1Multiplier", "0.02"}, // speed up smear effect; could be variable with limits
-    {"Field1Bias", "0, 0"}, // not variable: depends on field. Do we have any 0-1 fields that would need this?
-    {"Field2Multiplier", "0.01"}, // speed up smear effect; could be variable with limits
-    {"Field2Bias", "0, 0"}, // not variable: depends on field. Do we have any 0-1 fields that would need this?
-  });
-
   auto audioPaletteModPtr = synthPtr->addMod<SomPaletteMod>("Palette Creator", {
     {"Iterations", "2000"}, // not variable: set per performance? Lower settles quickly, which is useful for simple music
   });
@@ -45,12 +36,9 @@ std::shared_ptr<Synth> createSynthSoftCircle(glm::vec2 size) {
     {"maxSourcePoints", "600"}, // not variable: set per performance?
     {"clusters", "7"}, // could be a manual variable (or a cumulative event trigger)
   });
+  // <<< AUDIO, CLUSTERS AND PALETTE
   
-  auto polarClusterModPtr = synthPtr->addMod<ClusterMod>("Polar Clusters", {
-    {"maxSourcePoints", "600"}, // not variable: set per performance (?)
-    {"clusters", "5"}, // large impact: could be a manual variable (or a cumulative event trigger)
-  });
-  
+  // >>> LAYER PROCESSES
   auto fluidModPtr = synthPtr->addMod<FluidMod>("Fluid", {
     {"dt", "0.008"}, // could be a manual variable but changes need to be small for stability
     {"value:dissipation", "0.999"}, // could be a manual variable but changes need to be small for stability
@@ -60,127 +48,117 @@ std::shared_ptr<Synth> createSynthSoftCircle(glm::vec2 size) {
     {"velocity:iterations", "0.0"}, // could tie it to above to smooth things out?
     {"pressure:iterations", "25.0"}, // not variable
   });
-  
+
   auto fluidRadialImpulseModPtr = synthPtr->addMod<FluidRadialImpulseMod>("Cluster Impulses", {
     {"ImpulseRadius", "0.15"}, // could be variable
     {"ImpulseStrength", "0.015"}, // could be variable
   });
   
-  auto polarFluidRadialImpulseModPtr = synthPtr->addMod<FluidRadialImpulseMod>("Polar Cluster Impulses", {
-    {"ImpulseRadius", "0.1"}, // as above
-    {"ImpulseStrength", "0.03"}, // as above
+  auto smearModPtr = synthPtr->addMod<SmearMod>("Smear", {
+    {"Translation", "0.0, 0.0"}, // could be variable but unlikely to be useful
+    {"MixNew", "0.7"}, // 0.3 (slow) -> 0.9 (fast); could be variable. When this slows down, the multipliers become useful so they are coupled to some extent
+    {"AlphaMultiplier", "0.997"}, // fades fast when lower; could be variable with limits
+    {"Field1Multiplier", "0.02"}, // speed up smear effect; could be variable with limits
+    {"Field1Bias", "0, 0"}, // not variable: depends on field. Do we have any 0-1 fields that would need this?
+    {"Field2Multiplier", "0.01"}, // speed up smear effect; could be variable with limits
+    {"Field2Bias", "0, 0"}, // not variable: depends on field. Do we have any 0-1 fields that would need this?
   });
+  // <<< LAYER PROCESSES
   
-  auto radius1ModPtr = synthPtr->addMod<VaryingValueMod>("Radius 1", {
-    {"Mean", "0.05"},
-    {"Variance", "0.5"},
-    {"Min", "0.01"},
-    {"Max", "0.07"},
-  });
-
+  // >>> POINTS
   auto clusterPointsModPtr = synthPtr->addMod<SoftCircleMod>("Cluster Points", {
-    {"ColorMultiplier", "0.5"}, // could be variable
-    {"AlphaMultiplier", "0.2"}, // could be variable
+    {"Color Multiplier", "0.5"}, // could be variable
+    {"Alpha Multiplier", "0.2"}, // could be variable
     {"Softness", "0.8"}, // could be variable
   });
   
   auto pitchRmsPointsModPtr = synthPtr->addMod<SoftCircleMod>("Pitch RMS Points", {
-    {"ColorMultiplier", "0.4"},
-    {"AlphaMultiplier", "0.5"},
+    {"Color Multiplier", "0.4"},
+    {"Alpha Multiplier", "0.5"},
     {"Softness", "0.6"},
   });
   
-  auto radius2ModPtr = synthPtr->addMod<VaryingValueMod>("Radius 2", {
-    {"Mean", "0.01"},
-    {"Variance", "0.4"},
-    {"Min", "0.0"},
-    {"Max", "0.03"},
-  });
-
-  auto polarPitchRmsPointsModPtr = synthPtr->addMod<SoftCircleMod>("Polar Pitch RMS Points", {
-    {"Softness", "0.5"},
-    {"AlphaMultiplier", "0.3"},
-    {"ColorMultiplier", "0.4"},
-  });
-  
   auto polarSpectralPointsModPtr = synthPtr->addMod<SoftCircleMod>("Polar Spectral Points", {
+    {"Color Multiplier", "1.0"},
+    {"Alpha Multiplier", "0.2"},
     {"Softness", "1.0"},
-    {"AlphaMultiplier", "0.2"},
-    {"ColorMultiplier", "1.0"},
   });
+  // <<< POINTS
   
+  // >>> AUDIO, CLUSTERS AND PALETTE
   connectSourceToSinks(audioDataSourceModPtr, {
     { AudioDataSourceMod::SOURCE_SPECTRAL_3D_POINTS, {
       { audioPaletteModPtr, SomPaletteMod::SINK_VEC3 },
     }},
-    { AudioDataSourceMod::SOURCE_SPECTRAL_2D_POINTS, {
-      { polarSpectralPointsModPtr, SoftCircleMod::SINK_POINTS },
-    }},
     { AudioDataSourceMod::SOURCE_PITCH_RMS_POINTS, {
       { clusterModPtr, ClusterMod::SINK_VEC2 },
-      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_POINTS },
-      { pitchRmsPointsModPtr, SoftCircleMod::SINK_POINTS },
     }},
-    { AudioDataSourceMod::SOURCE_POLAR_PITCH_RMS_POINTS, {
-      { polarClusterModPtr, ClusterMod::SINK_VEC2 },
-      { polarFluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_POINTS },
-      { polarPitchRmsPointsModPtr, SoftCircleMod::SINK_POINTS },
+  });
+  connectSourceToSinks(audioPaletteModPtr, {
+    { SomPaletteMod::SOURCE_DARKEST_VEC4, {
+      { synthPtr, Synth::SINK_BACKGROUND_COLOR }
+    }},
+  });
+  // <<< AUDIO, CLUSTERS AND PALETTE
+  
+  // >>> LAYER PROCESSES
+  connectSourceToSinks(clusterModPtr, {
+    { ClusterMod::SOURCE_CLUSTER_CENTRE_VEC2, {
+      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_POINTS },
+    }}
+  });
+  connectSourceToSinks(audioDataSourceModPtr, {
+    { AudioDataSourceMod::SOURCE_PITCH_RMS_POINTS, {
+      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_POINTS },
     }},
     { AudioDataSourceMod::SOURCE_RMS_SCALAR, {
-      { radius1ModPtr, VaryingValueMod::SINK_MEAN },
+      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_IMPULSE_RADIUS }, // TODO: MulAdd
     }},
-    { AudioDataSourceMod::SOURCE_SPECTRAL_CREST_SCALAR, {
-      { polarPitchRmsPointsModPtr, SoftCircleMod::SINK_POINT_RADIUS },
-      { polarSpectralPointsModPtr, SoftCircleMod::SINK_POINT_RADIUS },
-    }},
-    { AudioDataSourceMod::SOURCE_ONSET1, {
-//      { clusterModPtr, ClusterMod::SINK_CHANGE_CLUSTER_NUM },
-//      { smearModPtr, SmearMod::SINK_CHANGE_LAYER },
-//      { polarPitchRmsPointsModPtr, SoftCircleMod::SINK_CHANGE_LAYER },
-//      { clusterPointsModPtr, SoftCircleMod::SINK_CHANGE_LAYER },
-//      { pitchRmsPointsModPtr, SoftCircleMod::SINK_CHANGE_LAYER },
-    }},
-//    { AudioDataSourceMod::SOURCE_TIMBRE_CHANGE, {
-//      { audioPaletteModPtr, SomPaletteMod::SINK_SWITCH_PALETTE }
-//    }},
-//    { AudioDataSourceMod::SOURCE_PITCH_CHANGE, {
-//    }},
-  });
-  connectSourceToSinks(radius1ModPtr, {
-    { VaryingValueMod::SOURCE_FLOAT, {
-      { clusterPointsModPtr, SoftCircleMod::SINK_POINT_RADIUS },
-      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_IMPULSE_RADIUS },
-    }}
   });
   connectSourceToSinks(audioPaletteModPtr, {
     { SomPaletteMod::SOURCE_FIELD, {
       { smearModPtr, SmearMod::SINK_FIELD_1_FBO },
     }},
-    { SomPaletteMod::SOURCE_RANDOM_VEC4, {
-      { clusterPointsModPtr, SoftCircleMod::SINK_POINT_COLOR },
+  });
+  // <<< LAYER PROCESSES
+  
+  // >>> POINTS
+  connectSourceToSinks(audioDataSourceModPtr, {
+    { AudioDataSourceMod::SOURCE_POLAR_SPECTRAL_2D_POINTS, {
+      { polarSpectralPointsModPtr, SoftCircleMod::SINK_POINTS },
     }},
-    { SomPaletteMod::SOURCE_RANDOM_DARK_VEC4, {
-      { pitchRmsPointsModPtr, SoftCircleMod::SINK_POINT_COLOR }
-    }},
-    { SomPaletteMod::SOURCE_RANDOM_LIGHT_VEC4, {
-      { polarPitchRmsPointsModPtr, SoftCircleMod::SINK_POINT_COLOR },
-    }},
-    { SomPaletteMod::SOURCE_DARKEST_VEC4, {
-      { synthPtr, Synth::SINK_BACKGROUND_COLOR }
+    { AudioDataSourceMod::SOURCE_PITCH_RMS_POINTS, {
+      { pitchRmsPointsModPtr, SoftCircleMod::SINK_POINTS },
     }},
   });
   connectSourceToSinks(clusterModPtr, {
     { ClusterMod::SOURCE_CLUSTER_CENTRE_VEC2, {
       { clusterPointsModPtr, SoftCircleMod::SINK_POINTS },
-      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_POINTS },
     }}
   });
-  connectSourceToSinks(polarClusterModPtr, {
-    { ClusterMod::SOURCE_CLUSTER_CENTRE_VEC2, {
-      { clusterPointsModPtr, SoftCircleMod::SINK_POINTS },
-//      { fluidRadialImpulseModPtr, FluidRadialImpulseMod::SINK_POINTS },
-    }}
+  connectSourceToSinks(audioDataSourceModPtr, {
+    { AudioDataSourceMod::SOURCE_SPECTRAL_CREST_SCALAR, {
+      { polarSpectralPointsModPtr, SoftCircleMod::SINK_POINT_RADIUS }, // TODO: MulAdd
+    }},
+    { AudioDataSourceMod::SOURCE_ZERO_CROSSING_RATE_SCALAR, {
+      { pitchRmsPointsModPtr, SoftCircleMod::SINK_POINT_RADIUS }, // TODO: MulAdd
+    }},
+    { AudioDataSourceMod::SOURCE_RMS_SCALAR, {
+      { clusterPointsModPtr, SoftCircleMod::SINK_POINT_RADIUS }, // TODO: MulAdd
+    }},
   });
+  connectSourceToSinks(audioPaletteModPtr, {
+    { SomPaletteMod::SOURCE_DARKEST_VEC4, {
+      { polarSpectralPointsModPtr, SoftCircleMod::SINK_POINT_COLOR }
+    }},
+    { SomPaletteMod::SOURCE_RANDOM_DARK_VEC4, {
+      { pitchRmsPointsModPtr, SoftCircleMod::SINK_POINT_COLOR }
+    }},
+    { SomPaletteMod::SOURCE_RANDOM_LIGHT_VEC4, {
+      { clusterPointsModPtr, SoftCircleMod::SINK_POINT_COLOR },
+    }},
+  });
+  // <<< POINTS
 
   // ***************************************************************************
   // TODO: need params for the layer fades
@@ -205,22 +183,17 @@ std::shared_ptr<Synth> createSynthSoftCircle(glm::vec2 size) {
   assignDrawingLayerPtrToMods(fluidDrawingLayerPtr, {
     { fluidModPtr },
     { clusterPointsModPtr },
-//    { pitchRmsPointsModPtr },
   });
   assignDrawingLayerPtrToMods(fluidVelocitiesDrawingLayerPtr, {
     { fluidModPtr, FluidMod::VELOCITIES_LAYERPTR_NAME },
     { fluidRadialImpulseModPtr },
-    { polarFluidRadialImpulseModPtr },
   });
   assignDrawingLayerPtrToMods(smearedDrawingLayerPtr, {
     { smearModPtr },
-//    { clusterPointsModPtr },
-//    { fluidRadialImpulseModPtr },
     { pitchRmsPointsModPtr },
-    { polarSpectralPointsModPtr },
   });
   assignDrawingLayerPtrToMods(fadeDrawingLayerPtr, {
-    { polarPitchRmsPointsModPtr },
+    { polarSpectralPointsModPtr },
   });
 
   // TODO: set up as DrawingLayers?
